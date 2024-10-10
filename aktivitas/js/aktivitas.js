70276204d30eaf9064efd11cbec45c7817eb8268
ChelaLorenchia <chela.lorenchia277@gmail.com>
2024-10-10 22:00:20 +0700

    update

--- aktivitas/js/aktivitas.js
+++ aktivitas/js/aktivitas.js
@@ -100,6 +100,39 @@ for (let i = 0; i < tabHeaderNodes.length; i++) {
 
         // Perbaikan interpolasi string pada style.left
         tabIndicator.style.left = `calc(${i * 20}% + 5px)`;
-    });
+    })
 }
+
+/*SWAIPER*/
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  //Pagination bullets
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+  },
+
+  //Navigation arrows
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+
+  //Responsive Breakpoints
+  breakpoints: {
+      0: {
+          slidesPerView: 1
+      },
+      768: {
+          slidesPerView: 2
+      },
+      1024: {
+          slidesPerView: 3
+      },
+  }
+  });
+
 
