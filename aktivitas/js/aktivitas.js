d4ba1ff4f7335f545a4f6e9f7d70c52aeaf8b560
lusykar1in4 <161692437+lusykar1in4@users.noreply.github.com>
2024-10-10 23:46:50 +0700

    update Raja Ampat

--- aktivitas/js/aktivitas.js
+++ aktivitas/js/aktivitas.js
@@ -81,3 +81,35 @@ const initSlider = (type) => {
     next.click(); // Menggunakan elemen tunggal
   }, 8000);
 };
+
+/* Raja Ampat */
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  //Pagination bullets
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+  },
+
+  //Navigation arrows
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+
+  //Responsive Breakpoints
+  breakpoints: {
+      0: {
+          slidesPerView: 1
+      },
+      768: {
+          slidesPerView: 2
+      },
+      1024: {
+          slidesPerView: 3
+      },
+  }
+});
